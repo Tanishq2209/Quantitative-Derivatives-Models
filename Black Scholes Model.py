f4591e869cb0ce1b9fd96f2e3aba636c76dda969
Tanishq Gupta <gu.tanishq@gmail.com>
2025-08-19 10:32:30 +0200

    Update Black Scholes Model.py

--- Black Scholes Model.py
+++ Black Scholes Model.py
@@ -1,1 +1,14 @@
+import numpy as np
+from scipy.stats import norm
 
+# Black-Scholes Model for European Call and Put options
+def european_BS(t, St, K, T, r, sigma, option='call'):
+    d1 = ((np.log(St / K) + (r + 0.5 * sigma**2) * (T - t))) / (sigma * np.sqrt(T - t))
+    d2 = d1 - sigma * np.sqrt(T - t)
+    
+    if option=='call':
+        return St * norm.cdf(d1) - K * np.exp(-r * (T - t)) * norm.cdf(d2)
+    elif option=='put':
+        return K * np.exp(-r * (T - t)) * norm.cdf(-d2) - St * norm.cdf(-d1)
+    else:
+        raise ValueError("Option type must be either call or put")
